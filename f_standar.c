c4334965dd5a3641a813a94e466adb4bb278cc67
Your Name <k.soto@pucp.pe>
2021-05-11 18:24:12 -0500

    functions of standard library

---
+++ f_standar.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ *_atoi - extract the number to integer
+ *@str: string input
+ *Return: number int
+ */
+int _atoi(char *str)
+{
+	int result = 0;
+	int sign = 1;
+	int digit;
+
+	if (*str == '-')
+	{
+		sign = -1;
+		++str;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10;
+		digit = *str - '0';
+		result = result + (digit * sign);
+		++str;
+	}
+	return (result);
+}
