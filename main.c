31a78455edbf87cbd72dfc0c41e8206837ff1292
kateincoding <kateincoding@gmail.com>
2021-05-11 22:56:44 -0500

    betty style

--- main.c
+++ main.c
@@ -4,54 +4,61 @@ var_t svar;
 
 void initialize_stack(void);
 
+/**
+ * main - entry point of the program
+ *
+ * @argc: argc
+ * @argv: arguments
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(int argc, char **argv)
 {
-    ssize_t line_len;
-    FILE *fd;
-    char *buff, *line, *opcode, *number;
-    size_t buff_size = 0;
+	ssize_t line_len;
+	FILE *fd;
+	char *buff, *line, *opcode, *number;
+	size_t buff_size = 0;
 
-    if (argc != 2)
-    {
-        write(2, "USAGE: monty file\n", 18);
-        exit(EXIT_FAILURE);
-    }
-    /* fopen returns a FILE pointer */
-    fd = fopen(argv[1], "r");
-    if (!fd)
-    {
-        /* it is similar than write but in printf , fd = 2 because is an error */
-        dprintf(2, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    initialize_stack();
-    /*in case we can't use the getline line_size = read_line(fd, &buff);*/
+	if (argc != 2)
+	{
+		write(2, "USAGE: monty file\n", 18);
+		exit(EXIT_FAILURE);
+	}
+	/* fopen returns a FILE pointer */
+	fd = fopen(argv[1], "r");
+	if (!fd)
+	{
+		/* it is similar than write but in printf , fd = 2 because is an error */
+		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	initialize_stack();
+	/*in case we can't use the getline line_size = read_line(fd, &buff);*/
 	line_len = getline(&buff, &buff_size, fd);
-    while (line_len >= 0)
-    {
-        svar.nodes_number++;
-        line = strtok(buff, "\n");
-        /* printf("line = %s\n", line); */
-        line = strtok(buff, "#");
-        opcode = strtok(buff, " ");
-        number = strtok(NULL, "\n");
-        /* printf("opcode = %s\n", opcode); */
-        /* printf("number = %s\n\n", number); */
-        pick_function(opcode, atoi(number));
+	while (line_len >= 0)
+	{
+		svar.nodes_number++;
+		line = strtok(buff, "\n");
+		/* printf("line = %s\n", line); */
+		line = strtok(buff, "#");
+		opcode = strtok(buff, " ");
+		number = strtok(NULL, "\n");
+		/* printf("opcode = %s\n", opcode); */
+		/* printf("number = %s\n\n", number); */
+		pick_function(opcode, atoi(number));
 
-        /* we need to check if inside of the line, there are an opcode and return it */
-        line_len = getline(&buff, &buff_size, fd);
-        line = NULL;
-    }
-    free(line);
-    free(buff);
-    return (0);
+		/* we need to check if inside of the line, there are an opcode and return it */
+		line_len = getline(&buff, &buff_size, fd);
+		line = NULL;
+	}
+	free(line);
+	free(buff);
+	return (0);
 }
 
 void initialize_stack(void)
 {
-    /* svar.type : 0 is stack ;  1 is queue */
-    svar.type = 0;
-    svar.head = NULL;
-    svar.nodes_number = 0;
+	/* svar.type : 0 is stack ;  1 is queue */
+	svar.type = 0;
+	svar.head = NULL;
+	svar.nodes_number = 0;
 }
