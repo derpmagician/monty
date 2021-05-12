0b297b531cac7d4c9b355a9a3de4088b1c5b1da8
Renzo <renzin@gmail.com>
2021-05-12 15:12:39 +0000

    changing spaces to tabs

--- op-add.c
+++ op-add.c
@@ -10,20 +10,20 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = *stack;
-    int sum = 0, idx = 0;
+	stack_t *tmp = *stack;
+	int sum = 0, idx = 0;
 
-    while(tmp->next)
-    {
-        tmp = tmp->next;
-        idx++;
-    }
+	while(tmp->next)
+	{
+		tmp = tmp->next;
+		idx++;
+	}
 
-    if (idx < 2)
-        handle_error(ERR_SWAP_USG, NULL, line_number, NULL);
+	if (idx < 2)
+		handle_error(ERR_SWAP_USG, NULL, line_number, NULL);
 
-    sum = (*stack)->n;
-    delete_dnodeint_at_index(stack, 0);
-    (*stack)->n += sum;
-    svar.nodes_number = svar.nodes_number - 1;
+	sum = (*stack)->n;
+	delete_dnodeint_at_index(stack, 0);
+	(*stack)->n += sum;
+	svar.nodes_number = svar.nodes_number - 1;
 }
