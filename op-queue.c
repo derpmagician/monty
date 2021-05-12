fc34b3e1172f74bcaff4dadd4c380997a5e7dbf9
kateincoding <kateincoding@gmail.com>
2021-05-12 10:40:26 -0500

    changing to queue mode

---
+++ op-queue.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * queue - stack FIFO: queue
+ * @stack: The head of the stack
+ * @line_number: The line where the operator is
+ *
+ * Return: Nothing
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+    (void) stack;
+    (void) line_number;
+
+    svar.type = 1;
+}
