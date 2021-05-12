99941e5f8dbabf3e1e053b365d60e91bf80cc56c
Renzo <renzin@gmail.com>
2021-05-12 13:03:29 +0000

    adding more errors

--- handle_err.c
+++ handle_err.c
@@ -99,3 +99,25 @@ void handle_uerror(int errno, unsigned int line)
 			break;
 	}
 }
+
+/**
+  * handle_more_uerror - Manages interpreter usage errors
+  * @errno: The error code to manage
+  * @line: The line on which the error occurred
+  *
+  * Return: Nothing
+  */
+void handle_more_uerror(int errno, unsigned int line)
+{
+	switch (errno)
+	{
+		case ERR_PCH_USG:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", line);
+			break;
+		case ERR_PCH_EMP:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
+			break;
+		default:
+			break;
+	}
+}
