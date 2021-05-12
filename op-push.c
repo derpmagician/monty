462983d61ef2ece02bc9ad2a0bad127e43a80437
kateincoding <kateincoding@gmail.com>
2021-05-12 15:40:02 -0500

    check first if it is a number

--- op-push.c
+++ op-push.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+int verify_number(char *number);
+
 /**
   * push - Adds a new node at the beginning of the stack
   * @stack: The head of the stack
@@ -7,16 +9,28 @@
   *
   * Return: Nothing
   */
-void push(stack_t **stack, unsigned int param)
+void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node = NULL;
+	int param;
 
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
 
-	new_node->n = param;
+	if (!verify_number(svar.after_opcode))
+	{
+		dprintf(2, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	/* maybe in the future, we will need an strtok */
+	if (svar.after_opcode[0] == '-')
+		param = -1 * atoi(svar.after_opcode);
+	else
+		param = atoi(svar.after_opcode);
 
+	new_node->n = param;
 	/* if is a stack or else: is a queue*/
 	if (svar.type == 0)
 		add_dnodeint(stack, param);
@@ -35,6 +49,22 @@ void push(stack_t **stack, unsigned int param)
 	new_node->next = *stack;
 	new_node->prev = NULL;
 	*stack = new_node; */
+}
+
+int verify_number(char *number)
+{
+	if (!number)
+		return (0);
+	if (*number == '-')
+		number++;
+
+	while (*number)
+	{
+		if (!isdigit(*number))
+			return (0);
+		number++;
+	}
+	return (1);
 }
 
 /**
