ebe4dc61bf96ecdd9fd12cc75b90e109df7a9b91
kateincoding <kateincoding@gmail.com>
2021-05-12 10:40:52 -0500

    stack

---
+++ op-stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * stack - stack LIFO: stack
+ * @stack: The head of the stack
+ * @line_number: The line where the operator is
+ *
+ * Return: Nothing
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+    (void) stack;
+    (void) line_number;
+
+    svar.type = 0;
+}
