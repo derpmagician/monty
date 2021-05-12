65fed068db98ae1076f5095c88ca809afffa9de2
kateincoding <kateincoding@gmail.com>
2021-05-12 02:49:27 -0500

    swap fixed, mem logical, changes since memory of stack or queue

--- op-swap.c
+++ op-swap.c
@@ -10,16 +10,30 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	unsigned int length = 0, temp = 0;
+	stack_t *tmp1; *tmp2;
 
-	length = count_stack(*stack);
+	/* length = count_stack(*stack); */
+	length = svar.nodes_number;
 
 	if (length < 2)
 		handle_error(ERR_SWAP_USG, NULL, line_number, NULL);
 
+	/* the change in monty is always since the memory, not values
 	if (*stack)
 	{
 		temp = (*stack)->n;
 		(*stack)->n = (*stack)->next->n;
 		(*stack)->next->n = temp;
 	}
+	*/
+	tmp1 = tmp2 = *stack;
+	tmp2 = tmp2->next;
+	/* first, will change the tmp1 to second position */
+	tmp1->next = tmp2->next;
+	(tmp2->next)->prev = tmp1;
+	tmp1->prev = tmp2;
+	/* first node and connection with second */
+	*head = tmp2;
+	tmp2->next = tmp1;
+	tmp2->prev = NULL;
 }
