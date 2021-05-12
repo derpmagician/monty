751aa78db2ee0ed400060e71a1b151c5e17992b2
kateincoding <kateincoding@gmail.com>
2021-05-12 15:40:23 -0500

    updated

--- pick_func.c
+++ pick_func.c
@@ -7,7 +7,6 @@
   * Return: A pointer to the function to be executed or
   * NULL if the function don't exists
   */
-/* void (*pick_func(char *s))(stack_t **, unsigned int) */
 void pick_function(char *opcode)
 {
 	instruction_t opcode_structure[] = {
@@ -27,7 +26,7 @@ void pick_function(char *opcode)
 		{ "rotl", rotl },
 		{ "rotr", rotr },
 		{ "stack", stack),
-		{ "queue", queue}} */
+		{ "queue", queue} */
 		{ NULL, NULL }
 	};
 	int i = 0;
@@ -39,10 +38,11 @@ void pick_function(char *opcode)
 		/* in here is better to return the function or just execute it */
 			/* printf("opcode tpok = %s\n", opcode); */
 			/* printf("number = %d\n", number); */
-			opcode_structure[i].f(&(svar.head), svar.n);
+			opcode_structure[i].f(&(svar.head), svar.nodes_number);
 			return;
 		}
 		++i;
 	}
+	dprintf(2, "Line %d: Unknow instruction %s\n", svar.nodes_number, opcode);
 	exit(EXIT_FAILURE);
 }
