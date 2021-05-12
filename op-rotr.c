5c94b3fdc99791dc3689fa098fe0b69a98a28215
kateincoding <kateincoding@gmail.com>
2021-05-12 11:42:03 -0500

    the last element of stack becomes the forst

--- op-rotr.c
+++ op-rotr.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * rotr - stack LIFO: stack
+ * rotr - the last element of stack becomes the first
  * @stack: The head of the stack
  * @line_number: The line where the operator is
  *
@@ -9,8 +9,18 @@
  */
 void rotr(stack_t **stack, unsigned int line_number)
 {
-    (void) stack;
-    (void) line_number;
+    stack_t *last_node, *tmp = *stack, *new_last;
 
-    svar.type = 0;
+    while (last_node->next)
+        last_node = last_node->next;
+
+    /* set the new last node */
+    new_last = last_node->prev;
+    new_last->next = last_node->next;
+    /* becoming the last node, first */
+    last_node->prev = tmp->prev;
+    last_node->next = tmp;
+    tmp->prev = last_node;
+    /* changing the stack header */
+    *stack = last_node;
 }
