5f5d799ad93bd0a7cbd700d73755fb5b78e9e5a7
kateincoding <kateincoding@gmail.com>
2021-05-12 03:45:16 -0500

    opcode -nop

---
+++ op-nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * add - adds the top two elements of the stack
+ * starting from the top of the stack.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void) stack;
+    (void) line_number;
+}
