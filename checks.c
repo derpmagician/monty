92227d21f5c3d69ecf47f514f92bca09375a7be0
Renzo <renzin@gmail.com>
2021-05-12 17:32:55 +0000

    adding some checks to open the file

---
+++ checks.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+  * check_args_num - Check the arguments passed on to the interpreter
+  * @argn: Number of args
+  *
+  * Return: Nothing
+  */
+void check_args_num(int argn)
+{
+	if (argn != MIN_ARGS)
+		handle_error(ERR_ARG_USG, NULL, 0, NULL);
+}
+
+/**
+  * check_access_rights - CChecks if the user has permissions to read the file
+  * @filename: The pathname of the file
+  *
+  * Return: Nothing
+  */
+void check_access_rights(char *filename)
+{
+	if (access(filename, R_OK) == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+}
