87502bfef4ef655f911b0c46c724015e021951fd
Renzo <renzin@gmail.com>
2021-05-13 21:32:44 +0000

    changing function

--- op-swap.c
+++ op-swap.c
@@ -9,31 +9,17 @@
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-	unsigned int length = 0; /* temp = 0; */
-	stack_t *tmp1, *tmp2;
+	unsigned int length = 0, temp = 0;
 
-	/* length = count_stack(*stack); */
-	length = svar.nodes_number;
+	length = count_stack(*stack);
 
 	if (length < 2)
 		handle_error(ERR_SWAP_USG, NULL, line_number, NULL);
 
-	/* the change in monty is always since the memory, not values*/
-	/*if (*stack)*/
-	/*{*/
-	/*	temp = (*stack)->n;*/
-	/*	(*stack)->n = (*stack)->next->n;*/
-	/*	(*stack)->next->n = temp;*/
-	/*}*/
-
-	tmp1 = tmp2 = *stack;
-	tmp2 = tmp2->next;
-	/* first, will change the tmp1 to second position */
-	tmp1->next = tmp2->next;
-	(tmp2->next)->prev = tmp1;
-	tmp1->prev = tmp2;
-	/* first node and connection with second */
-	*stack = tmp2;
-	tmp2->next = tmp1;
-	tmp2->prev = NULL;
+	if (*stack)
+	{
+		temp = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = temp;
+	}
 }
