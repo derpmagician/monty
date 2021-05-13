0ed48b8b0ada6fb27b0ea6b21b8f2f5afe6dc899
Renzo <renzin@gmail.com>
2021-05-13 17:49:51 +0000

    adding description

--- op-queue.c
+++ op-queue.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * queue - Pchange to fifo
+ * @stack: The head of the stack
+ * @line_number: current line
+ *
+ * Return: Nothing
+ */
 void queue(stack_t **stack, unsigned int line_number)
 {
 	(void) stack;
