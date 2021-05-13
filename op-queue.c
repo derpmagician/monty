478c736564b59574f2f4527508f662e024f6ea56
Renzo <renzin@gmail.com>
2021-05-13 23:16:12 +0000

    adding function

---
+++ op-queue.c
@@ -0,0 +1,14 @@
+/**
+ * queue - Pchange to fifo
+ * @stack: The head of the stack
+ * @line_number: current line
+ *
+ * Return: Nothing
+ */
+void queue(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	svar.type = 1;
+}
