bd254745a9a9ca8b4408d2908f7ebb39df680834
Your Name <k.soto@pucp.pe>
2021-05-13 14:35:51 -0500

    betty

--- op-push.c
+++ op-push.c
@@ -68,12 +68,7 @@ void push(stack_t **stack, unsigned int line_number)
 }
 
 /**
-  * push_queue - Adds a new node at the end of the stack
-  * @stack: The head of the stack
-  * @param: The value to adds on the stack
-  *
-  * Return: Nothing
-  * void push_queue(stack_t **stack, unsigned int param)
+* void push_queue(stack_t **stack, unsigned int param)
 * {
 *	stack_t *current = NULL, *new_node = NULL;
 *
