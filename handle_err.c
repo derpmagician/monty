1853d3f38789a277ab3b536ae0d9fd7387d57892
kateincoding <kateincoding@gmail.com>
2021-05-13 10:38:00 -0500

    change to fd 2

--- handle_err.c
+++ handle_err.c
@@ -63,37 +63,37 @@ void handle_uerror(int errno, unsigned int line)
 	switch (errno)
 	{
 		case ERR_ARG_USG:
-			fprintf(stderr, "USAGE: monty file\n");
+			dprintf(2, "USAGE: monty file\n");
 			break;
 		case ERR_PUSH_USG:
-			fprintf(stderr, "L%d: usage: push integer\n", line);
+			dprintf(2, "L%d: usage: push integer\n", line);
 			break;
 		case ERR_PINT_USG:
-			fprintf(stderr, "L%d: can't pint, stack empty\n", line);
+			dprintf(2, "L%d: can't pint, stack empty\n", line);
 			break;
 		case ERR_POP_USG:
-			fprintf(stderr, "L%d: can't pop an empty stack\n", line);
+			dprintf(2, "L%d: can't pop an empty stack\n", line);
 			break;
 		case ERR_SWAP_USG:
-			fprintf(stderr, "L%d: can't swap, stack too short\n", line);
+			dprintf(2, "L%d: can't swap, stack too short\n", line);
 			break;
 		case ERR_ADD_USG:
-			fprintf(stderr, "L%d: can't add, stack too short\n", line);
+			dprintf(2, "L%d: can't add, stack too short\n", line);
 			break;
 		case ERR_SUB_USG:
-			fprintf(stderr, "L%d: can't sub, stack too short\n", line);
+			dprintf(2, "L%d: can't sub, stack too short\n", line);
 			break;
 		case ERR_DIV_USG:
-			fprintf(stderr, "L%d: can't div, stack too short\n", line);
+			dprintf(2, "L%d: can't div, stack too short\n", line);
 			break;
 		case ERR_DIV_ZRO:
-			fprintf(stderr, "L%d: division by zero\n", line);
+			dprintf(2, "L%d: division by zero\n", line);
 			break;
 		case ERR_MUL_USG:
-			fprintf(stderr, "L%d: can't mul, stack too short\n", line);
+			dprintf(2, "L%d: can't mul, stack too short\n", line);
 			break;
 		case ERR_MOD_USG:
-			fprintf(stderr, "L%d: can't mod, stack too short\n", line);
+			dprintf(2, "L%d: can't mod, stack too short\n", line);
 			break;
 		default:
 			break;
