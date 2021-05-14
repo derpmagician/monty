3a049826908bc02a9d1939af852418db2fc23b73
kateincoding <kateincoding@gmail.com>
2021-05-13 20:28:33 -0500

    adding restriction is stack is different to null

--- op-pstr.c
+++ op-pstr.c
@@ -12,14 +12,13 @@ void pstr(stack_t **stack, unsigned int line_number)
 	stack_t *curr = *stack;
 	(void) line_number;
 
-	while (curr != NULL)
+	while (curr && stack)
 	{
-		if (curr->n == 0 || curr->n < 0 || curr->n > 127)
+		if (curr->n <= 0 || curr->n > 127)
 			break;
 
 		printf("%c", curr->n);
 		curr = curr->next;
 	}
-
 	printf("\n");
 }
